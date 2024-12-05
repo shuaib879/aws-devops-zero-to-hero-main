86f71b4196d9cb8e1b5ae283a19a99912d1e1026
shuaib879 <msraza879@gmail.com>
2024-12-05 11:42:50 +0530

    Update app.py-update

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world!-This is live update'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Listen on all IPs
